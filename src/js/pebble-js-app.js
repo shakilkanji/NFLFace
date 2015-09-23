a5a580d4a27620bd245e8553056545532a155b8d
Shakil Kanji <shakil.kanji@gmail.com>
2015-09-23 19:04:42 -0400

    fix: config page load

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,156 @@
+// Create a global variable for the user's desired team
+var desired_team = "SF";
+
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function getScores(pos) {
+  // Construct URL
+  var url = "http://www.nfl.com/liveupdate/scores/scores.json";
+
+  // Send request to nfl.com
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var scores = JSON.parse(responseText);
+
+      // Find desired game
+      function findGame(scores) {
+        for (var i =0; i < 16; i++) {
+          var game = Object.keys(scores)[i];
+          var game_string = String(game);
+          var home_team = scores[game_string].home.abbr;
+          if (String(home_team) == desired_team) {
+            return game_string;
+          }
+          var away_team = scores[game_string].away.abbr;
+          if (String(away_team) == desired_team) {
+            return game_string;
+          }
+        }
+      }
+      var desired_game_string = findGame(scores);
+      
+      // Home Team and Score
+      var home = scores[desired_game_string].home.abbr;
+      var home_score = scores[desired_game_string].home.score.T;
+
+      // Away Team and Score
+      var away = scores[desired_game_string].away.abbr;      
+      var away_score = scores[desired_game_string].away.score.T;
+      
+      // Game details
+      var network = scores[desired_game_string].media.tv;
+      
+      var month = desired_game_string.substring(4,6);
+      var day = desired_game_string.substring(6,8);
+      var date = month + "/" + day;
+      
+      var quarter = scores[desired_game_string].qtr;
+      var qtr_string = "Q" + quarter;
+      var time_left = scores[desired_game_string].clock;
+      var time_string = qtr_string + " " + time_left;
+      
+      var down = scores[desired_game_string].down;
+      var togo = scores[desired_game_string].togo;
+      var down_togo = down + " and " + togo;
+      
+      var yard_line = scores[desired_game_string].yl;
+      var posteam = scores[desired_game_string].posteam;
+
+      var info_string = posteam + " " + down_togo + " " + yard_line;
+      
+      // Assemble full strings to send
+      var home_info;
+      var away_info;
+      var network_info;
+      var date_info;
+      
+      // Conditions for Pregame, Ingame, Postgame
+      if (quarter === null) {
+        home_info = home;
+        away_info = away;
+        network_info = network;
+        date_info = date;
+      }
+      else if (quarter === "Pregame") {
+        home_info = home;
+        away_info = away;
+        network_info = network;
+        date_info = "Pregame";
+      }
+      else if (quarter === 1 || quarter === 2 || quarter === 3 || quarter === 4) {
+        home_info = home + " " + home_score;
+        away_info = away + " " + away_score;
+        network_info = info_string;
+        date_info = time_string;
+      }
+      else {
+        home_info = home + " " + home_score;
+        away_info = away + " " + away_score;
+        network_info = "FINAL";
+        date_info = "";
+      }
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+        "KEY_HOME_INFO": home_info,
+        "KEY_AWAY_INFO": away_info,
+        "KEY_NETWORK_INFO": network_info,
+        "KEY_DATE_INFO": date_info,
+        "KEY_DESIRED_TEAM": desired_team
+      };
+
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Scores sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending scores to Pebble!");
+        }
+      );
+    }      
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    // Get the initial scores
+    getScores();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    getScores();
+  }                     
+);
+
+// Listen for when the config page is opened
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'https://32acf9cc.ngrok.io/';
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+// Listen for when the config page is closed
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+  desired_team = configData.faveTeam;
+  getScores();
+});
---
+++ src/main.c
@@ -0,0 +1,350 @@
+#include <pebble.h>
+  
+#define KEY_HOME_INFO 0
+#define KEY_AWAY_INFO 1
+#define KEY_NETWORK_INFO 2
+#define KEY_DATE_INFO 3
+#define KEY_DESIRED_TEAM 4
+  
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_home_layer;
+static TextLayer *s_away_layer;
+static TextLayer *s_network_layer;
+static TextLayer *s_date_layer;
+
+static GFont s_time_font;
+static GFont s_scores_font;
+static GFont s_info_font;
+
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static bool twenty_four_hour_format = false;
+static char desired_team_buffer[16];
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  static char s_time_buffer[16];
+
+  if (twenty_four_hour_format) {
+    strftime(s_time_buffer, sizeof(s_time_buffer), "%H:%M", tick_time);
+  } else {
+    strftime(s_time_buffer, sizeof(s_time_buffer), "%I:%M", tick_time);
+  }
+  
+  if (!twenty_four_hour_format && s_time_buffer[0] == '0') {
+    memmove(s_time_buffer, &s_time_buffer[1], sizeof(s_time_buffer) - 1);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_time_buffer);
+}
+
+static uint32_t choose_resource_id() {
+  if (strcmp(desired_team_buffer, "ARI") == 0) {
+    return RESOURCE_ID_ARI;
+  }
+  else if (strcmp(desired_team_buffer, "ATL") == 0) {
+    return RESOURCE_ID_ATL;
+  }
+  else if (strcmp(desired_team_buffer, "BAL") == 0) {
+    return RESOURCE_ID_BAL;
+  }
+  else if (strcmp(desired_team_buffer, "BUF") == 0) {
+    return RESOURCE_ID_BUF;
+  }
+  else if (strcmp(desired_team_buffer, "CAR") == 0) {
+    return RESOURCE_ID_CAR;
+  }
+  else if (strcmp(desired_team_buffer, "CHI") == 0) {
+    return RESOURCE_ID_CHI;
+  }
+  else if (strcmp(desired_team_buffer, "CIN") == 0) {
+    return RESOURCE_ID_CIN;
+  }
+  else if (strcmp(desired_team_buffer, "CLE") == 0) {
+    return RESOURCE_ID_CLE2;
+  }
+  else if (strcmp(desired_team_buffer, "DAL") == 0) {
+    return RESOURCE_ID_DAL;
+  }
+  else if (strcmp(desired_team_buffer, "DEN") == 0) {
+    return RESOURCE_ID_DEN;
+  }
+  else if (strcmp(desired_team_buffer, "DET") == 0) {
+    return RESOURCE_ID_DET;
+  }
+  else if (strcmp(desired_team_buffer, "GB") == 0) {
+    return RESOURCE_ID_GB2;
+  }
+  else if (strcmp(desired_team_buffer, "HOU") == 0) {
+    return RESOURCE_ID_HOU;
+  }
+  else if (strcmp(desired_team_buffer, "IND") == 0) {
+    return RESOURCE_ID_IND2;
+  }
+  else if (strcmp(desired_team_buffer, "JAC") == 0) {
+    return RESOURCE_ID_JAC;
+  }
+  else if (strcmp(desired_team_buffer, "KC") == 0) {
+    return RESOURCE_ID_KC;
+  }
+  else if (strcmp(desired_team_buffer, "MIA") == 0) {
+    return RESOURCE_ID_MIA;
+  }
+  else if (strcmp(desired_team_buffer, "MIN") == 0) {
+    return RESOURCE_ID_MIN;
+  }
+  else if (strcmp(desired_team_buffer, "NE") == 0) {
+    return RESOURCE_ID_NE;
+  }
+  else if (strcmp(desired_team_buffer, "NO") == 0) {
+    return RESOURCE_ID_NO;
+  }
+  else if (strcmp(desired_team_buffer, "NYG") == 0) {
+    return RESOURCE_ID_NYG;
+  }
+  else if (strcmp(desired_team_buffer, "NYJ") == 0) {
+    return RESOURCE_ID_NYJ;
+  }
+  else if (strcmp(desired_team_buffer, "OAK") == 0) {
+    return RESOURCE_ID_OAK;
+  }
+  else if (strcmp(desired_team_buffer, "PHI") == 0) {
+    return RESOURCE_ID_PHI;
+  }
+  else if (strcmp(desired_team_buffer, "PIT") == 0) {
+    return RESOURCE_ID_PIT;
+  }
+  else if (strcmp(desired_team_buffer, "STL") == 0) {
+    return RESOURCE_ID_STL;
+  }
+  else if (strcmp(desired_team_buffer, "SD") == 0) {
+    return RESOURCE_ID_SD;
+  }
+  else if (strcmp(desired_team_buffer, "SEA") == 0) {
+    return RESOURCE_ID_SEA;
+  }
+  else if (strcmp(desired_team_buffer, "TB") == 0) {
+    return RESOURCE_ID_TB3;
+  }
+  else if (strcmp(desired_team_buffer, "TEN") == 0) {
+    return RESOURCE_ID_TEN;
+  }
+  else if (strcmp(desired_team_buffer, "WAS") == 0) {
+    return RESOURCE_ID_WAS;
+  }
+  else {
+    return RESOURCE_ID_SF;
+  }
+}
+
+static void main_window_load(Window *window) {
+  //Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(choose_resource_id());
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 0, 144, 46));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+  
+  // Create GFont: credit www.dafont.com
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_FRESHMAN_40));
+  
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  // Create Home Layer
+  s_home_layer = text_layer_create(GRect(0, 115, 72, 20));
+  text_layer_set_background_color(s_home_layer, GColorClear);
+  text_layer_set_text_color(s_home_layer, GColorWhite);
+  text_layer_set_text_alignment(s_home_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_home_layer, "");
+  
+  // Create Away Layer
+  s_away_layer = text_layer_create(GRect(72, 115, 72, 20));
+  text_layer_set_background_color(s_away_layer, GColorClear);
+  text_layer_set_text_color(s_away_layer, GColorWhite);
+  text_layer_set_text_alignment(s_away_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_home_layer, "");
+  
+  // Create Network Layer
+  s_network_layer = text_layer_create(GRect(0, 135, 144, 16));
+  text_layer_set_background_color(s_network_layer, GColorClear);
+  text_layer_set_text_color(s_network_layer, GColorWhite);
+  text_layer_set_text_alignment(s_network_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_home_layer, "No connection...");
+  
+  // Create Date Layer
+  s_date_layer = text_layer_create(GRect(0, 151, 144, 16));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_home_layer, "");
+  
+  // Create second custom font, apply it and add to Window
+  s_scores_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  s_info_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_14));
+  text_layer_set_font(s_home_layer, s_scores_font);
+  text_layer_set_font(s_away_layer, s_scores_font);
+  text_layer_set_font(s_network_layer, s_info_font);
+  text_layer_set_font(s_date_layer, s_info_font);
+
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_home_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_away_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_network_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  
+  // Make sure the time is displayed from the start
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_scores_font);
+  fonts_unload_custom_font(s_info_font);
+  
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
+  
+  // Destroy Time TextLayer
+  text_layer_destroy(s_time_layer);
+  
+  // Destroy NFL TextLayers
+  text_layer_destroy(s_home_layer);
+  text_layer_destroy(s_away_layer);
+  text_layer_destroy(s_network_layer);
+  text_layer_destroy(s_date_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+  
+  // Get scores update every 10 minutes
+  if(tick_time->tm_min % 10 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char home_layer_buffer[16];
+  static char away_layer_buffer[16];
+  static char network_layer_buffer[16];
+  static char date_layer_buffer[16];
+  
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_HOME_INFO:
+      snprintf(home_layer_buffer, sizeof(home_layer_buffer), "%s", t->value->cstring);
+      break;
+    case KEY_AWAY_INFO:
+      snprintf(away_layer_buffer, sizeof(away_layer_buffer), "%s", t->value->cstring);
+      break;
+    case KEY_NETWORK_INFO:
+      snprintf(network_layer_buffer, sizeof(network_layer_buffer), "%s", t->value->cstring);
+      break;
+    case KEY_DATE_INFO:
+      snprintf(date_layer_buffer, sizeof(date_layer_buffer), "%s", t->value->cstring);
+      break;
+    case KEY_DESIRED_TEAM:
+      snprintf(desired_team_buffer, sizeof(desired_team_buffer), "%s", t->value->cstring);
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Desired team set as %s", desired_team_buffer);
+      
+      // Reload the window to refresh background image
+      main_window_unload(s_main_window);
+      main_window_load(s_main_window);
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  
+  // Display full string
+  text_layer_set_text(s_home_layer, home_layer_buffer);
+  text_layer_set_text(s_away_layer, away_layer_buffer);
+  text_layer_set_text(s_network_layer, network_layer_buffer);
+  text_layer_set_text(s_date_layer, date_layer_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+  
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
